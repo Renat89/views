a8bee1a68147b425627d78845aae114edebbdbef
php 05 <php.05@metralha.cec.ime.usp.br>
2017-02-14 19:46:37 -0200

    Primeiro commit

---
+++ resources/views/ola.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Saudações!</title>
+  </head>
+  <body>
+      <div class="container">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                </thead>
+              </table> <!--table.table.table-striped>thead>tr>th*3 -->
+      </div>
+
+
+  </body>
+</html>
